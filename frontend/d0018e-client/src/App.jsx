80ae89ac265d9702df9f7df43ffd13f21d7ed504
engalv <tangbosin5@gmail.com>
2026-02-10 14:18:29 +0100

    Revert "schemes from mysqlworkbench"
    This reverts commit db38322c251f71a4c887c32af5130a1c197ae30a.

--- frontend/d0018e-client/src/App.jsx
+++ frontend/d0018e-client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="" target="_blank">
+        <a href="https://chatgpt.com" target="_blank">
           <img src={cdKungen} className="logo" alt="CDKungen logo" />
         </a>
       </div>
