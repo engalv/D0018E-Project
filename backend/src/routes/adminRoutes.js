672732f22b8033d13a599be3bfe8bc6951a8212b
engalv <tangbosin5@gmail.com>
2026-03-14 12:18:47 +0100

    Update adminRoutes.js

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { authenticate, requireAdmin } = require("./authMiddleware");
-const adminController = require("./adminController");
+const { authenticate, requireAdmin } = require("../authMiddleware");
+const adminController = require("../controllers/adminController");
 
 // Protect all admin routes
 router.use(authenticate);
